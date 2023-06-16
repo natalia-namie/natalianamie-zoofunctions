08363e43434ee8c41f9ad49b39529fb2dbb5159d
Natalia Namie <namiekomesso@gmail.com>
2023-06-16 14:50:41 -0300

    Resolução requisito 5 countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (animal = {}) => {
+  const { species: selectesSpecies, sex } = animal;
+
+  if (!selectesSpecies) {
+    const countAll = {};
+    species.forEach((speciesData) => {
+      countAll[speciesData.name] = speciesData.residents.length;
+    });
+    return countAll;
+  }
+  const speciesData = species.find((qSpecies) => qSpecies.name === selectesSpecies);
+  if (!speciesData) {
+    return 0;
+  }
+  if (sex) {
+    const count = speciesData.residents.filter((resident) => resident.sex === sex).length;
+    return count;
+  }
+  return speciesData.residents.length;
 };
+
+console.log(countAnimals());
+console.log(countAnimals({ species: 'penguins' }));
+console.log(countAnimals({ species: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
