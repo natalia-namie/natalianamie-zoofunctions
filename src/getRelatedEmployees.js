5a3901e188a77e671a8a4de65f8c33cb77f4b865
Natalia Namie <namiekomesso@gmail.com>
2023-06-16 12:02:35 -0300

    Resolução Requisito 4 getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,8 +3,6 @@ const { employees } = require('../data/zoo_data');
 const isManager = (id) => employees
   .some((employee) => employee.managers.some((manager) => manager === id));
 
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
-
 const getRelatedEmployees = (managerId) => {
   if (isManager(managerId)) {
     return employees
@@ -13,7 +11,5 @@ const getRelatedEmployees = (managerId) => {
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
-
-console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
