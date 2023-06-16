242ea2cd9c4351f61d4767c81de107552e6bc983
Natalia Namie <namiekomesso@gmail.com>
2023-06-16 01:02:33 -0300

    update requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => employees
+  .some((employee) => employee.managers.some((manager) => manager === id));
+
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees
+      .filter((employee) => employee.managers.some((manager) => manager === managerId))
+      .map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
+
+console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
