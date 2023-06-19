04ad75e94505fee20037e4d1b0f2a883bbda5617
Natalia Namie <namiekomesso@gmail.com>
2023-06-19 11:35:40 -0300

    Resolução Requisito 11 getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,30 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployeeObject = () => employees.map((employee) => ({
+  id: employee.id,
+  fullName: `${employee.firstName} ${employee.lastName}`,
+  species: species.filter((sp) => employee.responsibleFor.includes(sp.id))
+    .map((names) => names.name),
+  locations: species.filter((loc) => employee.responsibleFor.includes(loc.id))
+    .map((locations) => locations.location),
+}));
+
+const getEmployees = (empl) => { // get employees by name or id.
+  if (empl.name) {
+    return employees.some((n) => n.firstName === empl.name || n.lastName === empl.name);
+  } if (empl.id) {
+    return employees.some((id) => id.id === empl.id);
+  } throw new Error('Informações inválidas');
+};
+
+const getEmployeesCoverage = (empl) => {
+  if (!empl) {
+    return getEmployeeObject();
+  } if (getEmployees(empl)) {
+    const employee = employees
+      .find((e) => e.firstName === empl.name || e.lastName === empl.name || e.id === empl.id);
+    return getEmployeeObject().find((e) => e.id === employee.id);
+  } throw new Error('Informações inválidas');
 };
 
 module.exports = getEmployeesCoverage;
