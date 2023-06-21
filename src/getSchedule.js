24bd88a928a927a966795715ad1d28e94cd5eb3e
Natalia Namie <namiekomesso@gmail.com>
2023-06-20 21:49:59 -0300

    Requisito 9 a ser refatorado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+const weekDays = Object.keys(hours);
+const time = Object.values(hours);
+const info = {};
+
+const zooWeek = (scheduleTarget) => {
+  weekDays.forEach((weekDay, index) => {
+    const openingHour = `Open from ${time[index].open}am until ${time[index].close}pm`;
+    const speciesAv = species
+      .filter((name) => name.availability.includes(weekDay)).map((animal) => animal.name);
+
+    info[weekDay] = {
+      officeHour: scheduleTarget === 'Monday' ? 'CLOSED' : openingHour,
+      exhibition: scheduleTarget === 'Monday' ? 'The zoo will be closed!' : speciesAv,
+    };
+  });
+
+  return info;
+};
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (species.some((animal) => animal.name === scheduleTarget)) {
+    return species.filter((animal) => animal.name === scheduleTarget)[0].availability;
+  }
+
+  if (hours[scheduleTarget]) {
+    const days = {};
+    days[scheduleTarget] = zooWeek()[scheduleTarget];
+    return days;
+  }
+
+  return zooWeek();
 };
+
+console.log(getSchedule('lions'));
+console.log(getSchedule('Tuesday'));
+console.log(getSchedule('Monday'));
+console.log(getSchedule());
 
 module.exports = getSchedule;
